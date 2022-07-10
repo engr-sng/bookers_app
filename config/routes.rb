879cd67e303633599eb1fc812e211c94e7df9080
EC2 Default User <108569722+ShingoIshikawa@users.noreply.github.com>
2022-07-10 06:24:37 +0000

    [update]resourcesメソッドへ変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit'
-  patch 'books/:id' => 'books#update'
-  delete'books/:id' => 'books#destroy'
-
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
